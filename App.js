cfae2c278bbdeca934941fea726302f20e725111
Ananya Mohapatra <ananyamohapatra27@gmail.com>
2022-12-23 16:32:49 +0530

    Modified App.js

--- App.js
+++ App.js
@@ -1,67 +1,254 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   View,
   Text,
-  Button,
-  TouchableOpacity,
-  Linking
-} from 'react-native'
+  TextInput,
+} from 'react-native';
 
-const App= ()  => {
-  // const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
 
-  // const backgroundStyle = {
-  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  // };
+  const [name, SetName] = useState('');
 
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Ananya</Text>
-      <TouchableOpacity style={{ 
-                width:150,
-                borderWidth:2,
-                borderColor:'white',
-                backgroundColor:'white',
-                borderRadius:30,
-                justifyContent: 'center', 
-                alignItems:'center',
-                margin:10
-                }}
-                onPress={()=>{Linking.openURL('https://www.youtube.com/c/ProgrammingwithMash')}}><Text style={{color:'#000000'}}>View Youtube profile</Text></TouchableOpacity>
+      <Text style={styles.text}>
+        Please write your name:
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder='e.g. John'
+        onChangeText={(value) => SetName(value)}
+      />
+      <Text style={styles.text}>
+        Your name is: {name}
+      </Text>
     </View>
-   
   );
- };
+};
 
 const styles = StyleSheet.create({
- body:{
-  flex:1,
-  backgroundColor:'pink',
-  alignItems:'center',
-  justifyContent:'center'
- },
- text:{
-  color:'#ffff',
-  fontSize:25,
-  fontStyle:'italic'
- },
- button:{
-  width:150,
-                borderColor:'red',
-                borderRadius:30,
-                justifyContent: 'center', 
-                alignItems:'center'
- }
-
+  body: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#000000',
+    fontSize: 20,
+    margin: 10,
+  },
+  input: {
+    width: 200,
+    borderWidth: 1,
+    borderColor: '#555',
+    borderRadius: 5,
+    textAlign: 'center',
+    fontSize: 20,
+  },
 });
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Old one for practice
+
+
+// import React, { useState } from 'react';
+// import {
+//   StyleSheet,
+//   View,
+//   Text,
+//   RefreshControl,
+//   FlatList,
+//   SectionList,
+//   ScrollView,
+// } from 'react-native';
+
+// const App = () => {
+
+//   const [Items, setItems] = useState([
+//     { name: 'Item 1' },
+//     { name: 'Item 2' },
+//     { name: 'Item 3' },
+//     { name: 'Item 4' },
+//     { name: 'Item 5' },
+//     { name: 'Item 6' },
+//     { name: 'Item 7' },
+//     { name: 'Item 8' },
+//     { name: 'Item 9' },
+//     { name: 'Item 27' },
+//     { name: 'Item 78' },
+//   ]);
+//   const DATA = [
+//     {
+//       title: 'Title 1',
+//       data: ['Item 1-1', 'Item 1-2', 'Item 1-3'],
+//     },
+//     {
+//       title: 'Title 2',
+//       data: ['Item 2-1', 'Item 2-2', 'Item 2-3'],
+//     },
+//     {
+//       title: 'Title 3',
+//       data: ['Item 3-1'],
+//     },
+//     {
+//       title: 'Title 4',
+//       data: ['Item 4-1', 'Item 4-2'],
+//     },
+//   ]
+
+//   const [newArr,setNewArr]=useState([DATA[0]])
+//   const [index,setIndex]=useState(0)
+  
+//   const [Refreshing, setRefreshing] = useState(false);
+//   const onRefresh = () => {
+//     setRefreshing(true);
+//     let arr=[]
+//     if(index<3){
+      
+//       setIndex(index+1);
+//       let i=-1
+//       while(i<=index && index<3){
+//         arr.push(DATA[i+1])
+//         i++; 
+//       }
+//     }
+//          else{
+//           arr[0]=DATA[0];
+//           setIndex(0)      
+//          }
+//          setNewArr(arr)
+//          setRefreshing(false); 
+    
+//   }
+
+//   return (
+//       // <ScrollView nestedScrollEnabled={true}>
+//     <View style={styles.body}>
+//       <SectionList
+//       // keyExtractor={(item,index)=>index}
+//       // showsVerticalScrollIndicator={true}
+//       nestedScrollEnabled={true}
+//       sections={newArr}
+//       keyExtractor={(item, index) =>  index}
+//       // renderItem={({ item }) => <Item title={item} />}
+//       // renderSectionHeader={({ section: { title } }) => (
+//       //   <Text style={styles.header}>{title}</Text>
+//       // )}
+//       renderItem={(item)=>{
+//         return(
+//             <View style={styles.item}>
+//               {console.log("item",item)}
+//               <Text style={styles.item}>{item.item}</Text>
+//               </View>
+          
+//         )
+//       }}
+//       renderSectionHeader={(item)=>{
+//       return  (
+//           <View style={styles.item}>
+//             {console.log("item2",item)}
+//             <Text style={styles.text}>{item.section.title}</Text>
+//             </View>
+//         )
+//       }}
+      
+//       refreshControl={
+//           <RefreshControl
+//             refreshing={Refreshing}
+//             onRefresh={onRefresh}
+//             colors={['#ff00ff']}
+//           />
+//       }
+//       />
+//       {/* Using flatlist */}
+      
+//       {/* <FlatList
+//         // horizontal              //For horizontally viewing the items(But they are displayed by default from left to right)
+//         // inverted                //inverted will change the flow from right to left
+//         keyExtractor={(item, index) => index}
+//         data={Items}
+//         renderItem={({ item }) =>
+//         (
+//           <View style={styles.item}>
+//             <Text style={styles.text}>{item.name}</Text>
+//           </View>
+//         )
+//         }
+//         refreshControl={
+//           <RefreshControl
+//             refreshing={Refreshing}
+//             onRefresh={onRefresh}
+//             colors={['#ff00ff']}
+//           />
+//         }
+//       /> */}
+//     </View>
+//     // Using ScrollView
+//     // </ScrollView>
+//     // <ScrollView
+//     //   style={styles.body}
+//     //   refreshControl={
+//     //     <RefreshControl
+//     //       refreshing={Refreshing}
+//     //       onRefresh={onRefresh}
+//     //       colors={['#ff00ff']}
+//     //     />
+//     //   }
+//     // >
+//     //   {
+//     //     Items.map((object) => {
+//     //       return (
+//     //         <View style={styles.name} key={object.key}>
+//     //           <Text style={styles.text}>{object.item}</Text>
+//     //         </View>
+//     //       )
+//     //     })
+//     //   }
+//     // </ScrollView>
+//   );
+// };
+
